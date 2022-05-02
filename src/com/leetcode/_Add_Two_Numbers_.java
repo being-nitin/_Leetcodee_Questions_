993dabde9ecfd4497a064aeba3ec764e94015ac0
Nitin <nsinghboss02@gmail.com>
2022-05-02 22:10:16 +0530

    "Leetcode add two numbers problem"

--- src/com/leetcode/_Add_Two_Numbers_.java
+++ src/com/leetcode/_Add_Two_Numbers_.java
@@ -24,4 +24,34 @@ public class _Add_Two_Numbers_ {
     Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
     Output: [8,9,9,9,0,0,0,1]
      */
+    private class Node{
+        int val;
+        Node next;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.val = val;
+        }
+        public Node(int val,Node next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static Node sum(Node l1,Node l2){
+        Node head = l1;
+        int carryover = 0;
+        while(l1 != null){
+            l1.val = num1 + num2 + carryover;
+            carryover = l1.val / 10;
+            l1.val = l1.val - (carryOver * 10);
+
+            if(l1.next == null && (carryOver == 1 || l2.next != null)){
+                l1.next = new ListNode(carryOver);
+            }
+            l1 = l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        return head;
+    }
 }
